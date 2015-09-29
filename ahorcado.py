140fd56b54ddb3fd632da715e9edc354f4a212d6
soulreaper0711 <elliot.mata1@gmail.com>
2015-09-29 08:35:50 -0600

    ak7

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,3 @@
-## probando github con el manco dos veces al cuadrao
-
 import random
 
 intentosPermitidos = 7
@@ -28,14 +26,39 @@ def dibujarPalabra(palabra,letrasAcertadas):
             palabraConLetras = palabraConLetras + "_" + " "
     print(palabraConLetras)
 
-def juego():
-    intentosFallidos = 0
-    print(palabra)
-    letra = input("Digite una letra: ")
-    for i in palabra:
-        if letra in palabra:
-            dibujarPalabra(palabra,letrasAcertadas)
-        else:
-            intentosFallidos += 1
-            dibujarAhorcado(intentosFallidos)
-            juego()
+##def juego():
+##    intentosFallidos = 0
+##    print(palabra)
+##    letra = input("Digite una letra: ")
+##    while sigueJuego(intentosPermitidos,palabra,letrasFallidas,letrasAcertadas):
+##        if letra in palabra:
+##            dibujarPalabra(palabra,letrasAcertadas)
+##        else:
+##            intentosFallidos += 1
+##            dibujarAhorcado(intentosFallidos)
+##            juego()
+
+abecedario = "abcdefghijklmnñopqrstuvwxyz"
+
+def leerLetra(abecedario):
+    a = input("Digite una letra: ")
+    if a == "" or a not in abecedario:
+        print("Ingresó un caracter que no pertenece al abecedario, por favor intente de nuevo")
+        leerLetra(abecedario)
+    else:
+        if a in abecedario:
+            print("si")
+
+##while sigueJuego(intentosPermitidos,palabra,letrasFallidas,letrasAcertadas):
+##    letra = leerLetra(abecedario)
+##    if esta(letra,palabra):
+##        letrasAcertadas = agregar(letrasAcertadas,letra)
+##    else:
+##        letrasFallidas = letrasFallidas + letra
+##        dibujarAhorcado(len(letrasFallidas),intentosPermitidos)
+##        dibujarLetrasFallidas(letrasFallidas)
+##    dibujarPalabra(palabra,letrasAcertadas)
+##if gano(palabra,letrasAcertadas):
+##    resultado = "Gana"
+##else:
+##    resultado = "Pierde"
