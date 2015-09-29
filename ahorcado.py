68fbeb80e4064356563894ec0398f8d728242db3
soulreaper0711 <elliot.mata1@gmail.com>
2015-09-28 22:41:09 -0600

    Initial Commit

---
+++ ahorcado.py
@@ -0,0 +1,39 @@
+import random
+
+intentosPermitidos = 7
+letrasAcertadas = ""
+letrasFallidas = ""
+
+def escogerPalabraAlAzar():
+    palabras = "escrotony,mancalberto,perrantony,mancartola"
+    lista = palabras.split(",")
+    pos = random.randint(0,len(lista)-1)
+    return lista[pos]
+
+palabra = escogerPalabraAlAzar()
+
+def dibujarAhorcado(intentosFallidos):
+    nombreArchivo = "ahorcado"+str(intentosFallidos)+".txt"
+    archivo = open(nombreArchivo, "r")
+    print(archivo.read())
+
+def dibujarPalabra(palabra,letrasAcertadas):
+    palabraConLetras = ""
+    for letra in palabra:
+        if letra in letrasAcertadas:
+            palabraConLetras = palabraConLetras + letra + " "
+        else:
+            palabraConLetras = palabraConLetras + "_" + " "
+    print(palabraConLetras)
+
+def juego():
+    intentosFallidos = 0
+    print(palabra)
+    letra = input("Digite una letra: ")
+    for i in palabra:
+        if letra in palabra:
+            dibujarPalabra(palabra,letrasAcertadas)
+        else:
+            intentosFallidos += 1
+            dibujarAhorcado(intentosFallidos)
+            juego()
